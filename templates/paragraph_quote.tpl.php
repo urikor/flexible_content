ce7d919ffeec3e952a64731dc98f244bbef1ad6a
urikor <kikuchiku>
2016-07-28 14:20:20 +0300

    Feedback issues fix.

--- templates/paragraph_quote.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="quote">
-  <blockquote>
-    <?php print render($quote); ?>
-  </blockquote>
-  <?php if (!empty($link) && !empty($link_text)) : ?>
-    <a href="<?php print render($link); ?>"><?php print render($link_text); ?></a>
-  <?php endif; ?>
-</div>
